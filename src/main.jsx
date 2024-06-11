7f360a4fe21cc5dc9100bd902e50ac72310742dc
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2024-06-11 13:39:12 +0530

    fixed the className error in homeMain.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
 import App from "./components/app.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <App />
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
