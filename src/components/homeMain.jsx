cc76ea5c92a827f7f800dc7b2530de4857c1d38a
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2024-06-11 14:13:45 +0530

    Created a component for socialButtons.

--- src/components/homeMain.jsx
+++ src/components/homeMain.jsx
@@ -1,81 +1,58 @@
 import React from "react";
 import Typed from "typed.js";
+import { GitHubButton, TwitterButton } from "./socalButtons";
 
 function HomeMain() {
-    const el = React.useRef();
+  const el = React.useRef();
 
-    React.useEffect(() => {
-      const typed = new Typed(el.current, {
-        strings: [
-          "Full Stack Developer",
-          "iOS Developer",
-          "React Developer",
-          "Cloud Engineer",
-          "Gamer",
-        ],
-        typeSpeed: 100,
-        backDelay: 1800,
-        backSpeed: 90,
-        loop: true,
-      });
-  
-      return () => {
-        // Destroy Typed instance during cleanup to stop animation
-        typed.destroy();
-      };
-    }, []);
+  React.useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: [
+        "Full Stack Developer",
+        "iOS Developer",
+        "React Developer",
+        "Cloud Engineer",
+        "Gamer",
+      ],
+      typeSpeed: 100,
+      backDelay: 1800,
+      backSpeed: 90,
+      loop: true,
+    });
 
-    return (
-        <main>
-            <div className="main-child">
-            <img
-                className="profilePicture"
-                src="../../../public/images/Profile.jpg"
-            />
-            </div>
-            <div className="main-child main-info">
-            <h1>Manik Lakhanpal</h1>
-            <h2>
-                A <span id="typed" ref={el}></span>{" "}
-            </h2>
-            <p>
-                Hello, I'm Manik. I'm currently pursuing a Bachelor of Engineering
-                in Computer Science. I'm engaged in learning various subjects
-                simultaneously, including iOS development and Japanese language.
-                📚💻🌐
-            </p>
-            <div className="action-button">
-                <button type="button" className="social-button">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="17"
-                    height="17"
-                    fill="currentColor"
-                    className="social-github"
-                    viewBox="0 0 16 16"
-                >
-                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
-                </svg>
-                GitHub
-                </button>
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, []);
 
-                <button type="button" className="social-button">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="15"
-                    height="15"
-                    fill="currentColor"
-                    className="social-twitter"
-                    viewBox="0 0 16 16"
-                >
-                    <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
-                </svg>
-                Twitter
-                </button>
-            </div>
-            </div>
-        </main>
-    );
+  return (
+    <main>
+      <div className="main-child">
+        <img
+          className="profilePicture"
+          src="../../../public/images/Profile.jpg"
+        />
+      </div>
+      <div className="main-child main-info">
+        <h1>Manik Lakhanpal</h1>
+        <h2>
+          A <span id="typed" ref={el}></span>{" "}
+        </h2>
+        <p>
+          Hello, I'm Manik. I'm currently pursuing a Bachelor of Engineering in
+          Computer Science. 🎓💻 Alongside my studies, I'm exploring various
+          subjects 🌟, such as iOS development 📱 and the Japanese language 🗾.
+          I'm passionate about expanding my knowledge and skills in these areas.
+          📚✨
+        </p>
+        <div className="action-button">
+          <GitHubButton text="Github" />
+          <TwitterButton text="Twitter" />
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default HomeMain;
