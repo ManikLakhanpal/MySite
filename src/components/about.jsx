f156e05e707d4e83c50133c0dc36270f4d5b9566
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2024-06-14 12:16:46 +0530

    I added the about section.

---
+++ src/components/about.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function About() {
+    return (
+        <section className="about">
+            <h1>About</h1>
+            <div className="about-content">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate minima, aliquid error voluptates dolorum itaque temporibus perspiciatis fugit dolore nobis delectus tempore natus nam tenetur quis tempora vitae atque porro.
+            </div>
+        </section>
+    );
+}
+
+export default About;
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NavigationBar from "./navigationBar";
 import HomeMain from "./homeMain";
+import About from "./about";
 
 function App() {
   
@@ -8,6 +9,7 @@ function App() {
     <div>
       <NavigationBar />
       <HomeMain />
+      <About />
     </div>
   );
 }
