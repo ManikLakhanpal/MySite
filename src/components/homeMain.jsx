c4b525c6726295fb966645bff468e5fc3d0a8d47
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2024-07-11 09:54:32 +0530

    Fixed the issue with profile picture.

--- src/components/homeMain.jsx
+++ src/components/homeMain.jsx
@@ -31,7 +31,7 @@ function HomeMain() {
       <div className="main-child">
         <img
           className="profilePicture"
-          src="https://media.licdn.com/dms/image/D5603AQEWl0XJIItw8g/profile-displayphoto-shrink_400_400/0/1710506597763?e=1723680000&v=beta&t=SYNtrJfZ4ZChKztxDaM6pckwsgcXQHtmA6MCtbPonPc"
+          src="/images/Profile.jpeg"
         />
       </div>
       <div className="main-child main-info">
